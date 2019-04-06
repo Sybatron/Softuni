e65d4fee53950440c39e7e43e51420ee2735c55b
Sybatron <vipsister2014@gmail.com>
2019-04-06 19:23:15 +0300

    Added more of my unordered source codes

---
+++ Softuni/Exams/Tech_Exams/TechMidExam_10March19_Group1/P01SpringVacationTrip/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace P01SpringVacationTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var days = int.Parse(Console.ReadLine());
+            var budget = double.Parse(Console.ReadLine());
+            var people = int.Parse(Console.ReadLine());
+            var fuelPrice = double.Parse(Console.ReadLine());
+            var foodPrice = double.Parse(Console.ReadLine());
+            var hotelPrice = double.Parse(Console.ReadLine());
+            var currentExpenses = people * hotelPrice * days;
+
+            if (people > 10)
+            {
+                currentExpenses *= 0.75;
+            }
+            currentExpenses += people * foodPrice * days;
+
+            for (int i = 1; i <= days; i++)
+            {
+                //Every day, they travel some distance
+                //Every third and fifth day, they have some additional expenses, which are 40 % of the current value of the expenses.
+                //Every seventh day, their expenses are reduced, because they withdraw(receive) a small amount of money – you can calculate it by dividing the amount of the current expenses by the group of people.
+
+                var km = double.Parse(Console.ReadLine());
+                currentExpenses += km * fuelPrice;
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    currentExpenses *= 1.4;
+                }
+
+                if (i % 7 == 0)
+                {
+                    currentExpenses -= currentExpenses / people;
+                }
+
+                if (budget - currentExpenses < 0)
+                {
+                    Console.WriteLine($"Not enough money to continue the trip. You need {currentExpenses - budget:f2}$ more.");
+                    return;
+                }
+            }
+            Console.WriteLine($"You have reached the destination. You have {budget - currentExpenses:f2}$ budget left.");
+        }
+    }
+}
---
+++ Trash/Tekstove/Tekstove/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            countSav.Text = "0";
+            countDum.Text = "0";
+        }
+
+        private void doIt_Click(object sender, EventArgs e)
+        {
+            if (input.Text.Length > 0)
+            {
+                if (radioUpper.Checked)
+                {
+                    string TheOutput = input.Text;
+                    TheOutput = TheOutput.ToUpper();
+                    output.Text = TheOutput;
+                }
+                else if (radioLower.Checked)
+                {
+                    string TheOutput = input.Text;
+                    TheOutput = TheOutput.ToLower();
+                    output.Text = TheOutput;
+                }
+                else if (radioCount.Checked)
+                {
+                    int broi = 0;
+                    for (int i = 0; i < input.Text.Length - 1; i++)
+                    {
+                        if (input.Text[i] == ' ' && input.Text[i + 1] != ' ')
+                        {
+                            broi++;
+                        }
+                    }
+                    if (input.Text[input.Text.Length - 1] != ' ')
+                    {
+                        broi++;
+                    }
+                    countDum.Text = (broi).ToString();
+                }
+                else if (radioSearch.Checked)
+                {
+                    int index, broi = 0;
+                    string text = input.Text, textSearch = search.Text;
+                    index = text.IndexOf(textSearch);
+                    while (index != -1)
+                    {
+                        broi++; index = text.IndexOf(textSearch, index + 1);
+                    }
+                    countSav.Text = broi.ToString();
+                }
+                else if (radioReplace.Checked)
+                {
+                    output.Text = input.Text.Replace(search.Text, replace.Text);
+                }
+            }
+        }
+    }
+}
