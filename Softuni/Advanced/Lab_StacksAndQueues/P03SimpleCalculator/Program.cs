cdd71d031a0f9691ba66327bd6b8e1383798a2e7
Sybatron <vipsister2014@gmail.com>
2019-05-19 18:44:35 +0300

    Added Softuni_Advanced Course

---
+++ Softuni/Advanced/Lab_StacksAndQueues/P03SimpleCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ");
+            var stack = new Stack<string>(input);
+            var output = 0;
+            while (stack.Count > 1)
+            {
+                var num = int.Parse(stack.Pop());
+                var symbol = stack.Pop();
+                if (symbol == "+")
+                {
+                    output += num;
+                }
+                else
+                {
+                    output -= num;
+                }
+            }
+            output += int.Parse(stack.Pop());
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Softuni/Advanced/Lab_StacksAndQueues/P04MatchingBrackets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P04MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expr = Console.ReadLine();
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < expr.Length; i++)
+            {
+                if (expr[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (expr[i] == ')')
+                {
+                    var index = stack.Pop();
+                    Console.WriteLine(expr.Substring(index, i - index + 1));
+                }
+            }
+        }
+    }
+}
