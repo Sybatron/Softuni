a7a7a2296f1d2886dadbb50145cd6904c9241913
Sybatron <vipsister2014@gmail.com>
2019-08-11 13:14:37 +0300

    Added OOP Exam

---
+++ Softuni/Exams/OOP_Exams/OOPExam_11-August-2019/01. Structure_Skeleton/Project-Skeleton/ViceCity/Models/Players/CivilPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViceCity.Models.Players
+{
+    class CivilPlayer : Player
+    {
+        private const int lifePoints = 50;
+
+        public CivilPlayer(string name)
+            : base(name, lifePoints)
+        {
+        }
+    }
+}
---
+++ Softuni/Exams/OOP_Exams/OOPExam_11-August-2019/01. Structure_Skeleton/Project-Skeleton/ViceCity/Models/Players/MainPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViceCity.Models.Players
+{
+    class MainPlayer : Player
+    {
+        private const string name = "Tommy Vercetti";
+        private const int lifePoints = 100;
+
+        public MainPlayer()
+            : base(name, lifePoints)
+        {
+        }
+    }
+}
