1ec0366c7a4d831b040dbebad628606fa876dd7f
Sybatron <vipsister2014@gmail.com>
2019-12-04 09:25:31 +0200

    Add GSM class - School work 04-Dec-2019

---
+++ School/School_04-Dec-2019/MobilePhones/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MobilePhones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var defaultPhone = new GSM();
+            var customPhone = new GSM(
+                "Redmi Note 7",
+                "Xiaomi",
+                375.50,
+                new Battery("Non-removable Li-Po", "48 H", "5000 mAh"),
+                new Display(6, "24 M")
+                );
+
+            var wrongPhone = new GSM(
+                "Redmi Note 7",
+                "Xiaomi",
+                375.50,
+                new Battery("Non-removable Li-Po", "48 MAH", "5000 mAh"),
+                new Display(6, "24 M")
+                );
+
+            Console.WriteLine(defaultPhone + Environment.NewLine);
+            Console.WriteLine(customPhone + Environment.NewLine);
+            Console.WriteLine(wrongPhone);
+
+        }
+    }
+}
