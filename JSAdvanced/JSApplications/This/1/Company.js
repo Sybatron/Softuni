107d22392e1197f280df1114272f5754fe053218
Sybatron <vipsister2014@gmail.com>
2020-03-02 00:38:58 +0200

    Updated Company.js from Problem01 in JSApplications/This from JSAdvanced course

--- JSAdvanced/JSApplications/This/1/Company.js
+++ JSAdvanced/JSApplications/This/1/Company.js
@@ -1,8 +1,8 @@
 class Company {
     departments = [];
-    Employee = class EmployeeClass {
+    EmployeeClass = class Employee {
         constructor(username, salary, position, department) {
-            this.username = username;
+            this.username = username.toString();
             this.salary = salary;
             this.position = position;
             this.department = department;
@@ -15,6 +15,56 @@ class Company {
         if (salary < 0) {
             throw Error("Invalid input!");
         }
-        let employee = new this.Employee();
+        let employee = new this.EmployeeClass(username, salary, position, department);
+
+        this.departments.push(employee);
+    }
+
+    bestDepartment() {
+        this.departments = this.departments.sort(this.sortDepartments);
+        let highestAvg = 0;
+        let bestDepartmentName;
+        let index = 0;
+        while (index !== this.departments.length) {
+            let currentDepartmentName = this.departments[index].department;
+            let currentDepartments = this.departments.filter(empl => empl.department === currentDepartmentName);
+
+            let currentCount = currentDepartments.length;
+            let currentSum = currentDepartments.reduce((a, b) => a + b.salary, 0);
+            let currentAvg = currentSum / currentCount;
+            if (highestAvg < currentAvg) {
+                highestAvg = currentAvg;
+                bestDepartmentName = currentDepartmentName;
+            }
+            index = this.departments.map(el => el.department).lastIndexOf(currentDepartmentName) + 1;
+        }
+        //TODO
+        /** " Best Department is: {best department's name}
+         Average salary: {best department's average salary}
+         {employee1} {salary} {position}
+         {employee2} {salary} {position}
+         {employee3} {salary} {position} **/
+    }
+
+    sortDepartments(employee1, employee2) {
+        if (employee1.department !== employee2.department) {
+            return employee1.department.localeCompare(employee2.department);
+        }
+
+        if (employee1.salary !== employee2.salary) {
+            return employee2.salary - employee1.salary;
+        }
+
+        return employee1.username.localeCompare(employee2.username);
     }
 }
+
+let c = new Company();
+c.addEmployee("Stanimir", 2000, "engineer", "Construction");
+c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
+c.addEmployee("Slavi", 500, "dyer", "Construction");
+c.addEmployee("Stan", 2000, "architect", "Construction");
+c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
+c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
+c.addEmployee("Gosho", 1350, "HR", "Human resources");
+console.log(c.bestDepartment());
