d9e5923f3371062476b216fadab73216569480b8
Sybatron <vipsister2014@gmail.com>
2020-03-03 18:04:39 +0200

    Updated

---
+++ Softuni/DB/Entity Framework Core/Exercises_IntroductionToDBApps/P02VillainNames/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace P02VillainNames
+{
+    class Program
+    {
+        private static string connectionString =
+            "Server=.\\SQLEXPRESS;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true;";
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            using (connection)
+            {
+                var queryString = @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                        FROM Villains AS v 
+                                        JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                    GROUP BY v.Id, v.Name 
+                                      HAVING COUNT(mv.VillainId) > 3 
+                                    ORDER BY COUNT(mv.VillainId)";
+
+                var villainSelectComand = new SqlCommand(queryString, connection);
+
+                try
+                {
+                    var reader = villainSelectComand.ExecuteReader();
+
+                    Console.WriteLine("Comand was sucessful!");
+
+                    using(reader)
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["Name"]} - {reader["MinionsCount"]}");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Comand wasn't sucessful!");
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Softuni/DB/Entity Framework Core/Exercises_IntroductionToDBApps/P03MinionNames/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace P03MinionNames
+{
+    class Program
+    {
+        public static string connectionString =
+            "Server=.\\SQLEXPRESS;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true;";
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            var Id = int.Parse(Console.ReadLine());
+
+            using (connection)
+            {
+                var queryString = @"SELECT Name FROM Villains WHERE Id = @Id";
+
+                var sqlComand = new SqlCommand(queryString, connection);
+                sqlComand.Parameters.AddWithValue("@Id", Id);
+
+                try
+                {
+                    var name = sqlComand.ExecuteScalar();
+
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                
+
+            }
+        }
+    }
+}
