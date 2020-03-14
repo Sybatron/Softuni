504635209299483d82885691fbc7a462bb62ba07
Sybatron <vipsister2014@gmail.com>
2020-03-14 04:24:26 +0200

    REST and AJAX
    finished Problems 1-2
    I think they are 100/100

--- JSCourses/JSAdvanced/JSApplications/RESTServicesAndAJAX/solution/01.BUS-STOP/app.js
+++ JSCourses/JSAdvanced/JSApplications/RESTServicesAndAJAX/solution/01.BUS-STOP/app.js
@@ -1,3 +1,18 @@
 function getInfo() {
-    
+	let stopInputValue = document.querySelector('#stopId').value.trim();
+	let stopNameRef = document.querySelector('#stopName');
+	let stopBusesList = document.querySelector('#buses');
+	fetch(`https://judgetests.firebaseio.com/businfo/${stopInputValue}.json`).then((x) => x.json()).then((x) => {
+		stopBusesList.innerHTML = '';
+		if (!x.error) {
+			stopNameRef.innerHTML = x.name;
+			Object.entries(x.buses).forEach(([ busId, time ]) => {
+				let li = document.createElement('li');
+				li.innerHTML = `Bus ${busId} arrives in ${time}`;
+				stopBusesList.appendChild(li);
+			});
+		} else {
+			stopNameRef.innerHTML = 'Error';
+		}
+	});
 }
--- JSCourses/JSAdvanced/JSApplications/RESTServicesAndAJAX/solution/02.BUS-SCHEDULE/app.js
+++ JSCourses/JSAdvanced/JSApplications/RESTServicesAndAJAX/solution/02.BUS-SCHEDULE/app.js
@@ -1,17 +1,62 @@
 function solve() {
+	let connectionRef = document.querySelector('.info');
+	let currentStopName = '';
+	let nextStopId = '';
+	let departActionRef = document.querySelector('#depart');
+	let arriveActionRef = document.querySelector('#arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+	fetch(`https://judgetests.firebaseio.com/schedule.json`)
+		.then((x) => x.json())
+		.then((x) => console.log(x));
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+	function getScheduleById(id) {
+		let requestedId = id;
+		if (!id) {
+			requestedId = 'depot';
+		}
+		return fetch(
+			`https://judgetests.firebaseio.com/schedule/${requestedId}.json`
+		);
+	}
 
-    return {
-        depart,
-        arrive
-    };
+	function setStateToTravelling() {
+		departActionRef.disabled = true;
+		arriveActionRef.disabled = false;
+	}
+
+	function setStateToIdle() {
+		departActionRef.disabled = false;
+		arriveActionRef.disabled = true;
+	}
+
+	function setNextStop(stopName) {
+		connectionRef.innerHTML = `Next stop ${stopName}`;
+	}
+
+	function setArriving(stopName) {
+		connectionRef.innerHTML = `Arriving at ${stopName}`;
+	}
+
+	function depart() {
+		getScheduleById(nextStopId).then((x) => x.json()).then((x) => {
+			nextStopId = x.next;
+			currentStopName = x.name;
+			setNextStop(currentStopName);
+			setStateToTravelling();
+		});
+	}
+
+	function arrive() {
+		getScheduleById(nextStopId).then((x) => x.json()).then((x) => {
+			setArriving(currentStopName);
+			setStateToIdle();
+		});
+	}
+
+	return {
+		depart,
+		arrive
+	};
 }
 
 let result = solve();
