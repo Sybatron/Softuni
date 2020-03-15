43b95e9060fd61b4e0ea314c918bcbd717a7a70e
Sybatron <vipsister2014@gmail.com>
2020-03-15 04:44:59 +0200

    Problems finished
    - Finished problems 3-4
    - TODO:
    -- Sent REST and AJAX homework

--- JSCourses/JSAdvanced/JSApplications/RESTServicesAndAJAX/solution/03.PHONEBOOK/app.js
+++ JSCourses/JSAdvanced/JSApplications/RESTServicesAndAJAX/solution/03.PHONEBOOK/app.js
@@ -2,12 +2,104 @@ function attachEvents() {
 	let loadBtnRef = document.querySelector('#btnLoad');
 	let createBtnRef = document.querySelector('#btnCreate');
 
-	loadBtnRef.addEventListener('click', () => {
-		fetch('https://phonebook-nakov.firebaseio.com/phonebook.json')
+	let phonebookRef = document.querySelector('#phonebook');
+
+	let newPersonValue = document.querySelector('#person');
+	let newPhoneValue = document.querySelector('#phone');
+	let nextElementId;
+
+	let phonePattern = /^(\d{3})([-\s]{0,1}\d{3}){2}\d$/;
+
+	fetch('https://phonebook-nakov.firebaseio.com/phonebook.json')
+		.then((x) => x.json())
+		.then((x) => console.log(x));
+
+	function getAllPhones() {
+		return fetch('https://phonebook-nakov.firebaseio.com/phonebook.json')
 			.then((x) => x.json())
-			.then((x) => console.log(x));
+			.then((users) =>
+				Object.entries(users).filter(
+					(x) => !!x[1].person && !!x[1].phone
+				)
+			);
+	}
+
+	loadBtnRef.addEventListener('click', () => {
+		loadAllPhones();
 	});
-	console.log('TODO...');
+
+	createBtnRef.addEventListener('click', () => {
+		if (!newPersonValue.value || !newPhoneValue.value) {
+			alert('Blank input data!');
+			return;
+		}
+		if (!phonePattern.exec(newPhoneValue.value)) {
+			alert(
+				'The phone number is not in format: xxx-xxx-xxxx or xxx xxx xxxx or xxxxxxxxxx'
+			);
+			return;
+		}
+
+		let newPerson = {
+			person : newPersonValue.value,
+			phone  : newPhoneValue.value
+		};
+		getAllPhones().then((currentPhoneCollection) => {
+			let nextId;
+			if (!currentPhoneCollection[currentPhoneCollection.length - 2]) {
+				nextId = 1;
+			} else {
+				nextId =
+					parseInt(
+						currentPhoneCollection[
+							currentPhoneCollection.length - 2
+						][0]
+					) + 1;
+			}
+			fetch(
+				`https://phonebook-nakov.firebaseio.com/phonebook/${nextId}.json`,
+				{
+					method : 'PUT',
+					body   : JSON.stringify(newPerson)
+				}
+			)
+				.then(() => {
+					newPersonValue.value = '';
+					newPhoneValue.value = '';
+					loadAllPhones();
+				})
+				.catch((err) => {
+					console.log(err, ' <err');
+				});
+		});
+	});
+
+	function loadAllPhones() {
+		getAllPhones().then((usersToBeDisplayed) => {
+			nextElementId = usersToBeDisplayed.length + 1;
+			phonebookRef.innerHTML = '';
+			usersToBeDisplayed.forEach((user) => {
+				let li = document.createElement('li');
+				li.innerHTML = `${user[1].person}: ${user[1].phone}`;
+				li.id = 'phonebook';
+
+				let deleteBtn = document.createElement('button');
+				deleteBtn.innerHTML = 'Delete';
+				deleteBtn.addEventListener('click', () => {
+					fetch(
+						`https://phonebook-nakov.firebaseio.com/phonebook/${user[0]}.json`,
+						{
+							method : 'DELETE'
+						}
+					).then(loadAllPhones);
+				});
+
+				li.appendChild(deleteBtn);
+
+				phonebookRef.appendChild(li);
+			});
+		});
+	}
 }
 
 attachEvents();
--- JSCourses/JSAdvanced/JSApplications/RESTServicesAndAJAX/solution/04.MESSENGER/app.js
+++ JSCourses/JSAdvanced/JSApplications/RESTServicesAndAJAX/solution/04.MESSENGER/app.js
@@ -1,5 +1,72 @@
 function attachEvents() {
-    console.log('TODO...');
+	fetch('https://rest-messanger.firebaseio.com/messanger.json')
+		.then((x) => x.json())
+		.then((x) => console.log(x));
+
+	let authorInputRef = document.querySelector('#author');
+	let contentInputRef = document.querySelector('#content');
+
+	let messageArea = document.querySelector('#messages');
+
+	let refreshBtn = document.querySelector('#refresh');
+	let submitBtn = document.querySelector('#submit');
+
+	function getAllMessages() {
+		return fetch('https://rest-messanger.firebaseio.com/messanger.json')
+			.then((x) => x.json())
+			.then((messages) =>
+				Object.entries(messages).filter(
+					(x) => !!x[1].author && !!x[1].content
+				)
+			);
+	}
+
+	function refreshAllMessages() {
+		getAllMessages().then((messages) => {
+			messageArea.innerHTML = '';
+			messages.forEach((msg) => {
+				messageArea.innerHTML += `${msg[1].author}: ${msg[1]
+					.content}\n`;
+			});
+		});
+	}
+
+	refreshBtn.addEventListener('click', () => {
+		refreshAllMessages();
+	});
+
+	submitBtn.addEventListener('click', () => {
+		if (!authorInputRef.value && !contentInputRef.value) {
+			alert('Blank input data!');
+			return;
+		}
+		let message = {
+			author  : authorInputRef.value,
+			content : contentInputRef.value
+		};
+		fetch('https://rest-messanger.firebaseio.com/messanger.json', {
+			method : 'POST',
+			body   : JSON.stringify(message)
+		})
+			.then(() => {
+				authorInputRef.value = '';
+				contentInputRef.value = '';
+				refreshAllMessages();
+			})
+			.catch((err) => {
+				console.log(err, ' <err');
+			});
+	});
 }
 
 attachEvents();
+
+// (function deleteWrong() {
+// 	fetch(
+// 		`https://rest-messanger.firebaseio.com/messanger/-M2R0Dub8mvELXdFc -
+// 			ND.json`,
+// 		{
+// 			method : 'DELETE'
+// 		}
+// 	);
+// })()
